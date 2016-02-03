0819b2ccbc10fde1c40164cef7a4d48d0b21fccf
PhillMue <muenzuve@gmail.com>
2016-02-03 12:29:53 +0300

    completed Quickstart

---
+++ Quickstart/exceptions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+try:
+	fh = open('xlines.txt')
+	for line in fh.readlines():
+		print(line)
+except IOError as e:
+	print("something bad happened({})".format(e))
+
+
+print("after badness")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

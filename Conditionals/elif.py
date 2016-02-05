9780e100df0d5038dbee65e2cf88673339c644d7
PhillMue <muenzuve@gmail.com>
2016-02-05 07:47:33 +0300

    python elif

---
+++ Conditionals/elif.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+def main():
+	v = 4
+	if v == 1:
+		print("V is one")
+	elif v == 2:
+		print("V is two")
+	elif v == 3:
+		print("V is three")
+	elif v == 4:
+		print("V is four")
+	elif v == 5:
+		print("V is five")
+	elif v == 6:
+		print("V is six")
+	else:
+		print("We no count your number.")
+
+if __name__ == "__main__":
+	main()

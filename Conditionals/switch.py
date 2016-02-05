4f39975774e2c430102073ceb92a6e6bb128b355
PhillMue <muenzuve@gmail.com>
2016-02-05 07:49:10 +0300

    python switch

---
+++ Conditionals/switch.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+def main():
+	choices = dict(
+			one = "first",
+			two = "second",
+			three = "third",
+			four = "fourth",
+			five = "fifth",
+			six = "sixth",
+			seven = "seventh",
+			eight = "eighth",
+			nine = "ninth",
+			ten = "tenth "
+		)
+	v = "four"
+	print(choices.get(v, "other"))
+
+if __name__ == "__main__":
+	main()

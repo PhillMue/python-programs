f4f87d5ec51b77fe07f5113715ee2d4ccdf71e75
PhillMue <muenzuve@gmail.com>
2016-02-08 23:34:36 +0300

    complete

---
+++ operators/booleanops.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def main():
+	# And
+	print(True and False)
+	print(True and True)
+	print(False and False)
+	# Or
+	print(True or False)
+	print(False or True)
+	print(True or True)
+	print(False or False)
+
+if __name__ == "__main__":
+	main()
